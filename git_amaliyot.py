3f2b95b9b550d67fdde04788a49ea7aa6536128f
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-08-03 20:56:26 +0500

    Masala yechildi va yangi fayl qo'shildi

---
+++ git_amaliyot.py
@@ -0,0 +1,19 @@
+"""“raqamlar” nomli tekst fayl ichiga 1 dan 10 gacha 
+sonlarni kiritib, ularning yig'indisini ekranga 
+chiqaruvchi dastur yarating."""
+
+try:
+    with open("raqamlar.txt", "w") as fayl:
+        fayl.write("1 2 3 4 5 6 7 8 9 10")
+except FileNotFoundError:
+    print("Fayl ochishda xatolik!")
+
+try:
+    with open("raqamlar.txt", "r") as fayl:
+        mylist = fayl.read().split()
+        mylist2 = list(map(lambda x: int(x), mylist))
+        result = sum(mylist2)
+except FileNotFoundError:
+    print("Fayl ochishda xatolik!")
+
+print(result)
